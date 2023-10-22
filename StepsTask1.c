eef099351fc13b99cdf260f164e78782f66eb409
Ramith Gajjala <146209235+grramith@users.noreply.github.com>
2023-10-22 17:57:27 +0000

    commiting the coursework part 1

--- StepsTask1.c
+++ StepsTask1.c
@@ -44,5 +44,41 @@ void tokeniseRecord(const char *input, const char *delimiter,
 // Complete the main function
 int main() {
 
+    // Read in csv file FitnessData_2023.csv
+    // Declaring a pointer for the csv file
+    FILE *FitnessDataPointer; 
 
+    //Opening the csv file for Read-Only
+    char filename[] = "FitnessData_2023.csv";
+    FitnessDataPointer = fopen(filename,"r");
+
+    // Lets the customer know if the csv file is empty
+    if (FitnessDataPointer == NULL) {
+            printf("File is empty!");
+        }
+
+    // Store it in a suitably sized and structured array and typedef data structure
+    // Creating a suitably sized array of the typedef data structure FITNESS_DATA
+    FITNESS_DATA FitnessData[1000];
+
+    // New variable acts as a record counter
+    int record_count = 0;
+
+    // Read and parse the CSV data
+    while (fscanf(FitnessDataPointer, "%10[^,],%5[^,],%d\n", FitnessData[record_count].date, FitnessData[record_count].time, &FitnessData[record_count].steps) == 3) {
+        // Increment the record counter
+        record_count++;
+    }
+    // Close the file
+    fclose(FitnessDataPointer);
+
+    // Write the number of records in the file to the screen
+    printf("Number of records in file: %d\n", record_count);
+
+    // Print the first three rows of the csv file
+    for (int row = 0; row < 3; row++) {
+        printf("%s/%s/%d\n", FitnessData[row].date, FitnessData[row].time, FitnessData[row].steps);
+    }
+
+return 0;
 }
